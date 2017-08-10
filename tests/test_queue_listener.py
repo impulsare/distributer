8f3c7205f3d8eae9e0d92bf7378d01690e03643c
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-10 22:47:22 +0200

    First commit

---
+++ tests/test_queue_listener.py
@@ -0,0 +1,68 @@
+import os
+import signal
+import subprocess
+import unittest
+from impulsare_config import Reader as ConfigReader
+
+
+# https://docs.python.org/3/library/unittest.html#assert-methods
+class TestQueueListener(unittest.TestCase):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    base_cmd = ['queue-listener']
+
+    def test_requires_config(self):
+        res = self._exec_cmd(self.base_cmd)
+        self.assertIs(res['status'], 2)
+        self.assertEqual(res['stdout'], '')
+        self.assertRegex(res['stderr'], '.*Missing option "--host"')
+
+
+    def test_bad_host(self):
+        config = self._get_config()
+        cmd = self.base_cmd + ['-h', '127.0.0.1', '-p', '80', '-q', 'wrong']
+        res = self._exec_cmd(cmd)
+        self.assertIs(res['status'], 1, "Can't get status 1, message: {} ('{}')".format(res['stderr'], cmd))
+        self.assertEqual(res['stdout'], '')
+        self.assertRegex(res['stderr'], '.*Error 111 connecting to 127.0.0.1:80. Connection refused.*')
+        self.assertNotRegex(res['stderr'], '.*redis.exceptions.ConnectionError: Error 111 connecting to 127.0.0.1:80. Connection refused.*')
+
+
+    def test_bad_host_debug(self):
+        config = self._get_config()
+        cmd = self.base_cmd + ['--debug', '-h', '127.0.0.1', '-p', '80', '-q', 'wrong']
+        res = self._exec_cmd(cmd)
+        self.assertIs(res['status'], 1, "Can't get status 1, message: {} ('{}')".format(res['stderr'], cmd))
+        self.assertEqual(res['stdout'], '')
+        self.assertRegex(res['stderr'], '.*redis.exceptions.ConnectionError: Error 111 connecting to 127.0.0.1:80. Connection refused.*')
+
+
+    def test_right_config(self):
+        config = self._get_config()
+        cmd = self.base_cmd + ['-h', config['distributer']['host'], '-q', config['testqueue']['queue']]
+        res = self._exec_cmd(cmd)
+        self.assertIs(res['status'], 0, "Can't get status 0, message: {} ('{}')".format(res['stderr'], cmd))
+        self.assertEqual(res['stdout'], '')
+        self.assertRegex(res['stderr'], '.*RQ worker.*started')
+
+
+    def _get_config(self):
+        config_specs = self.base_path + '/../impulsare_distributer/static/specs.yml'
+        config_default = self.base_path + '/../impulsare_distributer/static/default.yml'
+
+        config = ConfigReader().parse(self.base_path + '/static/config_valid.yml', config_specs, config_default)
+
+        return config
+
+
+    def _exec_cmd(self, cmd: list):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            stdout, stderr = p.communicate(timeout=3)
+        except subprocess.TimeoutExpired:
+            p.terminate()
+            stdout, stderr = p.communicate()
+
+        stdout = stdout.decode().strip().replace('\n', '')
+        stderr = stderr.decode().strip().replace('\n', '')
+
+        return {'stdout': stdout, 'stderr': stderr, 'status': p.returncode}
---
+++ tests/test_queue_manager.py
@@ -0,0 +1,60 @@
+import unittest
+import os
+import redis
+import sys
+
+from impulsare_config import Reader
+from redis.exceptions import ConnectionError
+
+from impulsare_distributer import QueueManager
+base_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.abspath(base_dir + '/../'))
+
+
+def fake_print(message: str, job: str):
+    print('Message: {}'.format(message))
+    print('Job: {}'.format(job))
+
+
+# https://docs.python.org/3/library/unittest.html#assert-methods
+class TestQueueManager(unittest.TestCase):
+    def test_init_queue_invalid_conf(self):
+        with self.assertRaisesRegex(ValueError, "Your config is not valid: 'host' is a required property"):
+            QueueManager(base_dir + '/static/config_invalid.yml', 'testqueue')
+
+
+    def test_init_queue_missing_subkey_queue(self):
+        with self.assertRaisesRegex(KeyError, "You must have a key testqueue in your config with a sub-key queue"):
+            QueueManager(base_dir + '/static/config_missing_queue.yml', 'testqueue')
+
+
+    def test_queue_invalid_server(self):
+        with self.assertRaisesRegex(ConnectionError, "Error.*connecting to abc:6379.*"):
+            q = QueueManager(base_dir + '/static/config_wrong_server.yml', 'testqueue')
+            q.add('Hello world', fake_print, 'test')
+
+
+    def test_queue_valid(self):
+        config_file = base_dir + '/static/config_valid.yml'
+        specs_file = base_dir + '/../impulsare_distributer/static/specs.yml'
+        config = Reader().parse(config_file, specs_file).get('distributer')
+        con = redis.StrictRedis(host=config['host'])
+
+        # Clean
+        items = con.keys('rq:*')
+        for item in items:
+            con.delete(item)
+        items = con.keys('rq:*')
+        self.assertEqual(len(items), 0)
+
+        try:
+            q = QueueManager(config_file, 'testqueue')
+            job = q.add('Hello world', fake_print, 'test')
+        except ConnectionError:
+            print('Be careful to set the right server in config_valid')
+            sys.exit(0)
+
+        items = con.keys('rq:*')
+        self.assertGreater(len(items), 0)
+        self.assertIn(b'rq:queues', items)
+        self.assertIn(b'rq:job:' + job.id.encode(), items)
